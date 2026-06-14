afc9dff410c6d8fd60389e154bf545167cddf46f
Joshua Valladares <joshuadvalladares@gmail.com>
2026-06-13 22:31:43 -0400

    Initial commit: Core math and neural network libraries

---
+++ DataIterator.java
@@ -0,0 +1,148 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class DataIterator {
+    public int batchSize;
+    public int numBatches;
+    public double mean, standardDeviation;
+    private int batchCounter, index;
+    private boolean hasNextBatch;
+    private DataPair[] dataPairs;
+
+    public DataIterator(int batchSize, String dataPairsPath) throws Exception {
+        this(batchSize, loadDataPairs(dataPairsPath));
+    }
+
+    public DataIterator(int batchSize, DataPair[] data) throws Exception {
+        batchCounter = 0;
+        hasNextBatch = true;
+        index = 0;
+        this.dataPairs = Stream.of(data).map(x -> x.clone()).toArray(DataPair[]::new);
+        if (batchSize == 0) {
+            this.batchSize = data.length;
+        } else {
+            this.batchSize = batchSize;
+        }
+        this.numBatches = data.length / this.batchSize;
+        double[] meanAndStandardDeviation = StatisticalTesting.getMeanAndStandardDeviation(getData());
+        this.mean = meanAndStandardDeviation[0];
+        this.standardDeviation = meanAndStandardDeviation[1];
+        for (DataPair dataPair : dataPairs) {
+            dataPair.input.minus(this.mean).divide(this.standardDeviation).shape(1,
+                    dataPair.input.getRows() * dataPair.input.getColumns()); // normalize and flatten
+        }
+    }
+
+    public DataIterator() {
+
+    }
+
+    public Matrix[] getData() {
+        return Stream.of(dataPairs).map(x -> x.input).toArray(Matrix[]::new);
+    }
+
+    public Matrix[] getLabels() {
+        return Stream.of(dataPairs).map(x -> x.label).toArray(Matrix[]::new);
+    }
+
+    public void save(String saveFile) throws Exception {
+        Map<String, Object> map = new HashMap<>();
+        map.put("batchSize", batchSize);
+        map.put("numBatches", numBatches);
+        map.put("mean", mean);
+        map.put("standardDeviation", standardDeviation);
+        map.put("batchCounter", batchCounter);
+        map.put("index", index);
+        map.put("hasNextBatch", hasNextBatch);
+        List<Map<String, Object>> dataPairsList = Stream.of(dataPairs).map(x -> x.save()).toList();
+        map.put("dataPairs", dataPairsList);
+        FileOutputStream fileOut = new FileOutputStream(saveFile);
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        out.writeObject(map);
+        out.close();
+        fileOut.close();
+        return;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static DataIterator load(String saveFile) throws Exception {
+        Map<String, Object> data = null;
+        FileInputStream fileIn = new FileInputStream(saveFile);
+        ObjectInputStream in = new ObjectInputStream(fileIn);
+        data = (Map<String, Object>) in.readObject();
+        in.close();
+        fileIn.close();
+        DataIterator output = new DataIterator();
+        output.batchSize = (int) data.get("batchSize");
+        output.numBatches = (int) data.get("numBatches");
+        output.mean = (double) data.get("mean");
+        output.standardDeviation = (double) data.get("standardDeviation");
+        output.batchCounter = (int) data.get("batchCounter");
+        output.index = (int) data.get("index");
+        output.hasNextBatch = (boolean) data.get("hasNextBatch");
+        List<Map<String, Object>> dataPairsList = (List<Map<String, Object>>) data.get("dataPairs");
+        output.dataPairs = dataPairsList.stream().map(x -> DataPair.load(x)).toArray(DataPair[]::new);
+        return output;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static DataPair[] loadDataPairs(String dataPairsPath) throws Exception {
+        List<Map<String, Object>> temp;
+        FileInputStream fileIn = new FileInputStream(dataPairsPath);
+        ObjectInputStream in = new ObjectInputStream(fileIn);
+        temp = (List<Map<String, Object>>) in.readObject();
+        in.close();
+        fileIn.close();
+        Collections.shuffle(temp);
+        return temp.stream().map(x -> DataPair.load(x)).toArray(DataPair[]::new);
+    }
+
+    public DataPair[] nextBatch() throws Exception {
+        DataPair[] res = new DataPair[batchSize];
+        for (int i = 0; i < batchSize; i++) {
+            res[i] = dataPairs[index];
+            index++;
+        }
+        batchCounter++;
+        if (batchCounter == numBatches)
+            hasNextBatch = false;
+        return res;
+
+    }
+
+    public void reset() throws Exception {
+        batchCounter = 0;
+        hasNextBatch = true;
+        index = 0;
+    }
+
+    public boolean hasNextBatch() {
+        return hasNextBatch;
+    }
+
+    public int getBatchCounter() {
+        return batchCounter;
+    }
+
+    public DataPair get(int index) {
+        DataPair output = dataPairs[index].clone();
+        output.input.product(this.standardDeviation).add(this.mean);
+        return output;
+    }
+
+    public DataPair[] get(int startIndex, int endIndex) {
+        DataPair[] output = new DataPair[endIndex - startIndex];
+        for (int i = startIndex; i < endIndex; i++) {
+            output[i - startIndex] = this.get(i);
+        }
+        return output;
+    }
+
+}
---
+++ NeuralNetwork.java
@@ -0,0 +1,203 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+class NeuralNetwork {
+
+    String cost;
+    NodeLayer headNode, tailNode;
+
+    public NeuralNetwork(int inputNodes, String activation) throws Exception {
+        headNode = new NodeLayer(inputNodes, activation);
+        tailNode = headNode;
+    }
+
+    public NeuralNetwork() {
+
+    }
+
+    public void save(String saveFile) throws Exception {
+        Map<String, Object> data = new HashMap<>();
+        data.put("cost", cost);
+        List<Map<String, Object>> layers = new ArrayList<>();
+        NodeLayer temp = headNode;
+        while (temp != tailNode) {
+            layers.add(temp.save());
+            temp = temp.nextNodeLayer;
+        }
+        layers.add(temp.save());
+        data.put("layers", layers);
+        FileOutputStream fileOut = new FileOutputStream(saveFile);
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        out.writeObject(data);
+        out.close();
+        fileOut.close();
+        return;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static NeuralNetwork load(String filePath) throws Exception {
+        FileInputStream fileIn = new FileInputStream(filePath);
+        ObjectInputStream in = new ObjectInputStream(fileIn);
+        Map<String, Object> data = (Map<String, Object>) in.readObject();
+        in.close();
+        fileIn.close();
+        NeuralNetwork output = new NeuralNetwork();
+        output.cost = (String) data.get("cost");
+        List<Map<String, Object>> layers = (List<Map<String, Object>>) data.get("layers");
+        NodeLayer prior = NodeLayer.load(layers.get(0));
+        output.headNode = prior;
+        output.tailNode = prior;
+        NodeLayer next = null;
+        for (int i = 1; i < layers.size(); i++) {
+            next = NodeLayer.load(layers.get(i));
+            prior.nextNodeLayer = next;
+            next.previousNodeLayer = prior;
+            prior = next;
+        }
+        output.tailNode = next;
+        return output;
+    }
+
+    public NeuralNetwork add(int inputNodes, String activation) throws Exception {
+        NodeLayer newTail = new NodeLayer(inputNodes, activation);
+        tailNode.nextNodeLayer = newTail;
+        newTail.previousNodeLayer = tailNode;
+        tailNode = newTail;
+        return this;
+    }
+
+    public NeuralNetwork add(int outputNodes) throws Exception {
+        if (tailNode == null | tailNode.activation.equals("")) {
+            throw new Exception("You can only add an output layer once!");
+        }
+        return this.add(outputNodes, "");
+    }
+
+    public void compile(String cost) throws Exception {
+        this.cost = cost;
+        NodeLayer temp = headNode;
+        while (temp != tailNode) {
+            temp.compile();
+            temp = temp.nextNodeLayer;
+        }
+    }
+
+    public void printStructure() {
+        NodeLayer temp = headNode;
+        System.out.println("--------------------------------------");
+        System.out.println("-->Input layer with " + temp.numNodes + " nodes using " + temp.activation);
+        temp = temp.nextNodeLayer;
+        while (temp != tailNode) {
+            System.out.println(
+                    "-------> Hidden layer with " + temp.numNodes + " nodes using " + temp.activation);
+            temp = temp.nextNodeLayer;
+        }
+        System.out.println("--> Output layer with " + temp.numNodes + " nodes");
+        System.out.println("Cost Function: " + cost);
+        System.out.println("--------------------------------------");
+    }
+
+    public Matrix compute(Matrix input) throws Exception {
+        NodeLayer temp = headNode;
+        temp.feed(input);
+        while (temp != tailNode) {
+            temp.propogate();
+            temp = temp.nextNodeLayer;
+        }
+        return temp.values.clone();
+    }
+
+    public Matrix[] compute(Matrix[] input) throws Exception {
+        Matrix[] output = new Matrix[input.length];
+        for (int i = 0; i < output.length; i++) {
+            output[i] = compute(input[i]);
+        }
+        return output;
+    }
+
+    public void train(DataIterator trainer, double learningRate) throws Exception {
+        while (trainer.hasNextBatch()) {
+            resetAverages();
+            for (DataPair pair : trainer.nextBatch()) {
+                Matrix output = compute(pair.input);
+                backPropogateErrors(output, pair.label);
+                updateAverages();
+            }
+            updateParameters(-learningRate / (double) trainer.batchSize);
+        }
+        trainer.reset();
+        return;
+    }
+
+    public double validate(DataIterator validator) throws Exception {
+        double totalErrorSum = 0;
+        while (validator.hasNextBatch())
+            for (DataPair pair : validator.nextBatch()) {
+                totalErrorSum += getCost(compute(pair.input), pair.label);
+            }
+        double AverageCost = totalErrorSum / (double) (validator.numBatches * validator.batchSize);
+        validator.reset();
+        return AverageCost;
+    }
+
+    public double getClassifierAccuracy(DataIterator iter) throws Exception {
+        int count = 0;
+        int correct = 0;
+        while (iter.hasNextBatch()) {
+            for (DataPair pair : iter.nextBatch()) {
+                count++;
+                if (compute(pair.input).maxIndex() == pair.label.maxIndex()) {
+                    correct++;
+                }
+            }
+        }
+        iter.reset();
+        return 100. * (double) correct / (double) count;
+    }
+
+    private double getCost(Matrix output, Matrix expected) throws Exception {
+        return output.cost(expected, cost, 0).getSum() / (double) (output.getColumns() * output.getRows());
+    }
+
+    private void resetAverages() {
+        NodeLayer temp = headNode;
+        while (temp != tailNode) {
+            temp.biasAverages.zero();
+            temp.weightAverages.zero();
+            temp = temp.nextNodeLayer;
+        }
+    }
+
+    private void backPropogateErrors(Matrix output, Matrix expected) throws Exception {
+        NodeLayer temp = tailNode;
+        temp.backPropogateErrorsTailNode(output, expected, cost);
+        temp = temp.previousNodeLayer;
+        while (temp != headNode) {
+            temp.backPropogateErrors();
+            temp = temp.previousNodeLayer;
+        }
+    }
+
+    private void updateAverages() throws Exception {
+        NodeLayer temp = headNode;
+        while (temp != tailNode) {
+            temp.updateAverages();
+            temp = temp.nextNodeLayer;
+        }
+    }
+
+    private void updateParameters(double scalar) throws Exception {
+        NodeLayer temp = headNode;
+        while (temp != tailNode) {
+            temp.updateParameters(scalar);
+            temp = temp.nextNodeLayer;
+        }
+    }
+
+}
